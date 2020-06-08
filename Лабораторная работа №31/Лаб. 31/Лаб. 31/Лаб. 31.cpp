6e0339722c7e8f641158d17230f12191f2225dc1
Polina-Eremeeva29 <63774078+Polina-Eremeeva29@users.noreply.github.com>
2020-06-08 19:02:45 +0500

    Add files via upload

---
+++ Лабораторная работа №31/Лаб. 31/Лаб. 31/Лаб. 31.cpp
@@ -0,0 +1,362 @@
+﻿#include <iostream>
+#include <glut.h>
+#include <string>
+#include <vector>
+#include <queue>
+
+using namespace std;
+
+int WinW;
+int WinH;
+static int maxSize = 20;
+int R;
+struct vertCoord
+{
+	int x, y;
+};
+vertCoord vertC[20];
+template <class T>
+class Graph
+{
+private:
+	vector <int> vertList;
+	T adjMatrix[20][20];
+public:
+	Graph();
+	~Graph();
+	int GetVertPos(const T& vertex);
+	bool IsEmpty();
+	bool IsFull();
+	int GetAmountEdges();
+	int GetWeight(const T& vertex1, const T& vertex2);
+	vector<T> GetNbrs(const T& vertex);
+	void InsertVertex(const T& vertex);
+	void InsertEdge(const T& vertex1, const T& vertex2, int weight);
+	void Print();
+	void DrawGraph();
+};
+Graph<int>graph;
+template<class T>
+Graph<T>::Graph() {
+	for (int i = 0; i < maxSize; ++i) {
+		for (int j = 0; j < maxSize; ++j) {
+			this->adjMatrix[i][j] = 0;
+		}
+	}
+}
+template<class T>
+Graph<T>::~Graph() {}
+template<class T>
+int Graph<T>::GetVertPos(const T& vertex) {
+	for (int i = 0; i < this->vertList.size(); ++i) {
+		if (this->vertList[i] == vertex)
+			return i;
+	}
+	return -1;
+}
+template<class T>
+bool Graph<T>::IsEmpty() {
+	if (this->vertList.size() != 0)
+		return false;
+	else
+		return true;
+}
+template<class T>
+bool Graph<T>::IsFull() {
+	return (vertList.size() == maxSize);
+}
+template<class T>
+int Graph<T>::GetAmountEdges() {
+	int amount = 0;
+	if (!this->IsEmpty()) {
+		for (int i = 0, vertListSize = this->vertList.size();
+			i < vertListSize; ++i) {
+			for (int j = 0; j < vertListSize; ++j) {
+				if (this->adjMatrix[i][j] ==
+					this->adjMatrix[j][i] &&
+					this->adjMatrix[i][j] != 0)
+					amount += 1;
+			}
+		}
+		return (amount / 2);
+	}
+	else
+		return 0;
+}
+template<class T>
+int Graph<T>::GetWeight(const T& vertex1, const T& vertex2) {
+	if (!this->IsEmpty()) {
+		int vertPos1 = GetVertPos(vertex1);
+		int vertPos2 = GetVertPos(vertex2);
+		return adjMatrix[vertPos1][vertPos2];
+	}
+	return 0;
+}
+template<class T>
+std::vector<T> Graph<T>::GetNbrs(const T& vertex) {
+	std::vector<T> nbrsList;
+	int vertPos = this->GetVertPos(vertex);
+	if (vertPos != (-1)) {
+		for (int i = 0, vertListSize = this->vertList.size(); i < vertListSize; ++i) {
+			if (this->adjMatrix[vertPos][i] != 0 &&
+				this->adjMatrix[i][vertPos] != 0)
+				nbrsList.push_back(this->vertList[i]);
+
+		}
+	}
+	return nbrsList;
+}
+template<class T>
+void Graph<T>::InsertVertex(const T& vertex) {
+	if (!this->IsFull()) {
+		this->vertList.push_back(vertex);
+	}
+	else {
+		cout << "Граф уже заполнен. Невозможно добавить новую вершину " << endl;
+		return;
+	}
+}
+template<class T>
+void Graph<T>::InsertEdge(const T& vertex1, const T& vertex2, int weight) {
+	if (this->GetVertPos(vertex1) != (-1) && this->GetVertPos(vertex2) != (-1)) {
+		int vertPos1 = GetVertPos(vertex1);
+		int vertPos2 = GetVertPos(vertex2);
+		if (this->adjMatrix[vertPos1][vertPos2] != 0
+			&& this->adjMatrix[vertPos2][vertPos1] != 0) {
+			cout << "Ребро между вершинами уже есть" << endl;
+			return;
+		}
+		else {
+			this->adjMatrix[vertPos1][vertPos2] = weight;
+			this->adjMatrix[vertPos2][vertPos1] = weight;
+		}
+	}
+	else {
+		cout << "Обеих вершин (или одной из них) нет в графе " << endl;
+		return;
+	}
+}
+template<class T>
+void Graph<T>::Print() {
+	if (!this->IsEmpty()) {
+		cout << "Матрица смежности графа: " << endl;
+		for (int i = 0, vertListSize = this->vertList.size(); i < vertListSize; ++i) {
+			cout << this->vertList[i] << " ";
+			for (int j = 0; j < vertListSize; ++j) {
+				cout << " " << this->adjMatrix[i][j] << " ";
+			}
+			cout << endl;
+		}
+	}
+	else {
+		cout << "Граф пуст " << endl;
+	}
+}
+void drawCircle(int x, int y, int R)
+{
+	glColor3f(1.0, 1.0, 1.0);
+	float x1, y1;
+	glBegin(GL_POLYGON);
+	for (int i = 0; i < 360; i++)
+	{
+		float theta = 2.0f * 3.1415926f * float(i) / float(360);
+		y1 = R * cos(theta) + y;
+		x1 = R * sin(theta) + x;;
+		glVertex2f(x1, y1);
+	}
+	glEnd();
+
+	glColor3f(0.0f, 0.0f, 0.0f);
+	float x2, y2;
+	glBegin(GL_LINE_LOOP);
+	for (int i = 0; i < 360; i++)
+	{
+		float theta = 2.0f * 3.1415926f * float(i) / float(360);
+		y2 = R * cos(theta) + y;
+		x2 = R * sin(theta) + x;
+		glVertex2f(x2, y2);
+	}
+	glEnd();
+}
+void drawText(int nom, int x1, int y1)
+{
+	GLvoid* font = GLUT_BITMAP_HELVETICA_18;
+	string s = to_string(nom);
+	glRasterPos2i(x1 - 5, y1 - 5);
+	for (int j = 0; j < s.length(); j++)
+		glutBitmapCharacter(font, s[j]);
+}
+void drawLine(int text, int x0, int y0, int x1, int y1)
+{
+	glColor3i(0, 0, 0);
+	glBegin(GL_LINES);
+	glVertex2i(x0, y0);
+	glVertex2i(x1, y1);
+	glEnd();
+	drawText(text, (x0 + x1) / 2 + 10, (y0 + y1) / 2 + 10);
+
+	glEnd();
+}
+void drawVertex(int n)
+{
+	for (int i = 0; i < n; i++) {
+		drawCircle(vertC[i].x, vertC[i].y, R);
+		drawText(i + 1, vertC[i].x, vertC[i].y);
+	}
+}
+void setCoord(int i, int n)
+{
+	int R_;
+	int x0 = WinW / 2;
+	int y0 = WinH / 2;
+	if (WinW > WinH)
+	{
+		R = 5 * (WinH / 13) / n;
+		R_ = WinH / 2 - R - 10;
+	}
+	else {
+		R = 5 * (WinW / 13) / n;
+		R_ = WinW / 2 - R - 10;
+	}
+	float theta = 2.0f * 3.1415926f * float(i) / float(n);
+	float y1 = R_ * cos(theta) + y0;
+	float x1 = R_ * sin(theta) + x0;
+
+	vertC[i].x = x1;
+	vertC[i].y = y1;
+}
+int longest;
+Graph<int> makeGraph()
+{
+	Graph<int> graph;
+	int amountVerts, amountEdges, sourceVertex, targetVertex, edgeWeight;
+	cout << "Введите количество вершин графа: "; cin >> amountVerts; cout << endl;
+	cout << "Введите количество ребер графа: "; cin >> amountEdges; cout << endl;
+	for (int i = 1; i <= amountVerts; ++i) {
+		int* vertPtr = &i;
+		graph.InsertVertex(*vertPtr);
+	}
+	int n = amountVerts, m = 1, k = amountEdges;
+
+	vector<int> winner(m);
+	for (int i = 0; i < m; i++)
+	{
+		winner[i] = 6;
+		winner[i]--;
+	}
+	vector<vector<vector<int>>> path(n);
+	for (int i = 0; i < amountEdges; ++i) {
+		int a, b, t;
+		cout << "Исходная вершина: "; cin >> sourceVertex; cout << endl;
+		int* sourceVertPtr = &sourceVertex;
+		cout << "Конечная вершина: "; cin >> targetVertex; cout << endl;
+		int* targetVertPtr = &targetVertex;
+
+		cout << "Вес ребра: "; cin >> edgeWeight; cout << endl;
+		graph.InsertEdge(*sourceVertPtr, *targetVertPtr, edgeWeight);
+
+		a = sourceVertex, b = targetVertex, t = edgeWeight;
+		a--;
+		b--;
+		path[a].push_back({ b, t });
+		path[b].push_back({ a, t });
+	}
+	int start = 1;
+	start--;
+	vector<int> count(n, -1);
+	count[start] = 0;
+	queue<int> plan;
+	plan.push(start);
+	while (!plan.empty())
+	{
+		start = plan.front();
+		plan.pop();
+		for (auto next : path[start])
+		{
+			if (count[next[0]] == -1 || count[next[0]] > count[start] + next[1])
+			{
+				plan.push(next[0]);
+				count[next[0]] = count[start] + next[1];
+			}
+		}
+	}
+	bool delivered = true;
+	longest = 0;
+	for (int i = 0; i < m && delivered; i++)
+	{
+		longest = max(count[winner[i]], longest);
+	}
+	cout << endl;
+	graph.Print();
+	return graph;
+}
+void reshape(int w, int h)
+{
+	WinW = w;
+	WinH = h;
+	glViewport(0, 0, (GLsizei)WinW, (GLsizei)WinH);
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	gluOrtho2D(0, (GLdouble)WinW, 0, (GLdouble)WinH);
+	glutPostRedisplay();
+}
+void display()
+{
+	glShadeModel(GL_SMOOTH);
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	gluOrtho2D(0, WinW, 0, WinH);
+	glViewport(0, 0, WinW, WinH);
+	glClearColor(1.0, 1.0, 1.0, 1.0);
+	glClear(GL_COLOR_BUFFER_BIT);
+
+	graph.DrawGraph();
+
+	glutSwapBuffers();
+}
+template<class T>
+void Graph<T>::DrawGraph()
+{
+	int n = vertList.size();
+	for (int i = 0; i < n; i++)
+	{
+		setCoord(i, n);
+	}
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = i + 1; j < n; j++)
+		{
+			int a = adjMatrix[i][j];
+			if (a != 0)
+			{
+				drawLine(a, vertC[i].x, vertC[i].y, vertC[j].x, vertC[j].y);
+			}
+			if (a == adjMatrix[j][i] && a != 0)
+				drawLine(a, vertC[j].x, vertC[j].y, vertC[i].x, vertC[i].y);
+		}
+	}
+	drawVertex(n);
+}
+void func()
+{
+	cout << endl << "Коротчайший путь: " << longest;
+}
+int main(int argc, char* argv[])
+{
+	setlocale(LC_ALL, "rus");
+	glutInit(&argc, argv);
+
+	graph = makeGraph();
+
+	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA);
+	glutInitWindowSize(900, 720);
+	glutCreateWindow("Graph");
+	WinW = glutGet(GLUT_WINDOW_WIDTH);
+	WinH = glutGet(GLUT_WINDOW_HEIGHT);
+	glutDisplayFunc(display);
+	func();
+	glutReshapeFunc(reshape);
+	glutMainLoop();
+
+	return 0;
+}
